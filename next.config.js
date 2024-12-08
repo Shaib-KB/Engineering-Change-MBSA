96f0d4c0547cc8600a98dc59f301d80cb5dd7724
kwanele <kwanelebuthelezi2001@gmail.com>
2024-12-08 01:21:37 +0100

    v

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  reactStrictMode: true,
+  images: {
+    domains: ["example-apis.vercel.app"],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
